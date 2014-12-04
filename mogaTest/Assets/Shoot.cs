4b15ca76ef604d2e545eb9aae849c1df8e3ae22e
countspatula <mcpherson206@gmail.com>
2014-12-04 11:40:12 +0000

    Initial Commit

---
+++ mogaTest/Assets/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public float nextShot;
+    public float shotCooldown = 1;
+	// Use this for initialization
+	void Start () {    
+        nextShot = Time.time;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Moga_Input.GetKey(KeyCode.A))
+        {
+            if (Time.time > nextShot)
+            {
+                Instantiate(bullet, gameObject.transform.position, gameObject.transform.rotation);
+                nextShot = Time.time + shotCooldown;
+            }
+        }
+	}
+}
